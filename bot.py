8bfd9889bbb3ea7e1d9130f6dd31888ca0282f10
Minji Kang <fun3440@naver.com>
2018-12-20 08:23:34 +0000

    chatbot도입

---
+++ bot.py
@@ -0,0 +1,22 @@
+import os 
+import requests
+import json
+ 
+token = os.getenv('TELE_TOKEN')
+method = 'getUpdates'
+
+# url="https://api.telegram.org/bot{}/{}".format(token,method)
+url = "https://api.hphk.io/telegram/bot{}/{}".format(token,method)
+res = requests.get(url).json()
+
+user_id = res ["result"] [0] ["message"] ["from"] ["id"]
+
+msg = "머먹을래?"
+
+method='sendMessage'
+
+
+msg_url="https://api.hphk.io/telegram/bot{}/{}?chat_id={}&text={}".format(token,method,user_id,msg)
+print(msg_url)
+
+requests.get(msg_url)
